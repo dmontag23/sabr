048f9118edf26e62bbd5bb043747540de3ec198f
Dillon Montag <dmontag23@gmail.com>
2026-05-03 21:34:35 +0100

    Correct name for merge check

--- infra/github/main.tf
+++ infra/github/main.tf
@@ -38,7 +38,7 @@ resource "github_repository_ruleset" "default_branch_protection" {
       strict_required_status_checks_policy = true
 
       required_check {
-        context = "Pre-commit / Run pre-commit"
+        context = "Pre-commit / Run pre-commit (pull_request)"
       }
     }
   }
