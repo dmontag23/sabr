afc7a43aacb43e3f74cf754780c203f66b56951b
Dillon Montag <dmontag23@gmail.com>
2026-05-03 19:16:26 +0100

    Ensure branches are deleted on merge

--- infra/github/main.tf
+++ infra/github/main.tf
@@ -1,4 +1,5 @@
 resource "github_repository" "sabr" {
-  name        = "sabr"
-  description = "A finance app for the people you share life with."
+  name                   = "sabr"
+  description            = "A finance app for the people you share life with."
+  delete_branch_on_merge = true
 }
