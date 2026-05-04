d01f5049f0292916f97f1e404652e60732256316
Dillon Montag <dmontag23@gmail.com>
2026-05-04 17:46:26 +0100

    Create staging environment in supabase

--- infra/hcp-terraform/sabr.tf
+++ infra/hcp-terraform/sabr.tf
@@ -34,6 +34,28 @@ resource "tfe_variable" "github_token" {
   value_wo_version = 1
 }
 
+resource "tfe_workspace" "supabase_staging" {
+  name              = "sabr-supabase-staging"
+  description       = "Staging Supabase project for the sabr app."
+  organization      = tfe_organization.sabs_apps.name
+  project_id        = tfe_project.sabr.id
+  working_directory = "infra/supabase"
+  trigger_patterns  = ["infra/supabase/**/*"]
+  tag_names         = ["stack:sabr-supabase"]
+  vcs_repo {
+    identifier                 = "dmontag23/sabr"
+    github_app_installation_id = data.tfe_github_app_installation.gha_installation.id
+  }
+}
+
+resource "tfe_variable" "supabase_staging_project_name" {
+  key          = "project_name"
+  category     = "terraform"
+  workspace_id = tfe_workspace.supabase_staging.id
+  description  = "Supabase project name for staging environment."
+  value        = "sabr-staging"
+}
+
 resource "tfe_workspace" "supabase_prod" {
   name              = "sabr-supabase-prod"
   description       = "Production Supabase project for the sabr app."
@@ -48,16 +70,29 @@ resource "tfe_workspace" "supabase_prod" {
   }
 }
 
+resource "tfe_variable" "supabase_prod_project_name" {
+  key          = "project_name"
+  category     = "terraform"
+  workspace_id = tfe_workspace.supabase_prod.id
+  description  = "Supabase project name for production environment."
+  value        = "sabr-prod"
+}
+
 variable "supabase_organization_id" {
   type = string
 }
 
+variable "supabase_access_token" {
+  type      = string
+  ephemeral = true
+  sensitive = true
+}
 
 resource "tfe_variable_set" "supabase_shared_variables" {
   name          = "sabr-supabase-shared-variables"
   description   = "Shared variables for the sabr Supabase workspaces"
   organization  = tfe_organization.sabs_apps.name
-  workspace_ids = [tfe_workspace.supabase_prod.id]
+  workspace_ids = [tfe_workspace.supabase_staging.id, tfe_workspace.supabase_prod.id]
 }
 
 resource "tfe_variable" "supabase_organization_id" {
@@ -65,22 +100,17 @@ resource "tfe_variable" "supabase_organization_id" {
   category        = "terraform"
   variable_set_id = tfe_variable_set.supabase_shared_variables.id
   description     = "Supabase organization slug."
-  value           = var.supabase_organization_id
+
+  value = var.supabase_organization_id
 }
 
-variable "supabase_prod_access_token" {
-  type      = string
-  ephemeral = true
-  sensitive = true
-}
+resource "tfe_variable" "supabase_access_token" {
+  key             = "SUPABASE_ACCESS_TOKEN"
+  category        = "env"
+  sensitive       = true
+  variable_set_id = tfe_variable_set.supabase_shared_variables.id
+  description     = "Supabase access token."
 
-resource "tfe_variable" "supabase_prod_access_token" {
-  key          = "SUPABASE_ACCESS_TOKEN"
-  sensitive    = true
-  category     = "env"
-  workspace_id = tfe_workspace.supabase_prod.id
-  description  = "Production Supabase personal access token."
-
-  value_wo         = var.supabase_prod_access_token
+  value_wo         = var.supabase_access_token
   value_wo_version = 1
 }
