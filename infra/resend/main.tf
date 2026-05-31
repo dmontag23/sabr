0d6e88be21661a250e987ca35858d9f7cae020dc
Dillon Montag <dmontag23@gmail.com>
2026-05-31 12:34:56 +0100

    Add resend domain

--- infra/resend/main.tf
+++ infra/resend/main.tf
@@ -1,4 +1,11 @@
+resource "resend_domain" "sabr" {
+  name   = "notifications.unlikely.ai"
+  region = "eu-west-1"
+  tls    = "enforced"
+}
+
 resource "resend_api_key" "supabase_smtp_api_key" {
   name       = "sabr-supabase-smtp-integration"
   permission = "sending_access"
+  domain_id  = resend_domain.sabr.id
 }
