d9f9719b186cbaa76dd22bf7dc2f3fd2223255c0
Dillon Montag <dmontag23@gmail.com>
2026-06-02 12:09:41 +0100

    Change SUPABASE_PROJECT_ID from secret to env variable

--- infra/github/secrets.tf
+++ infra/github/secrets.tf
@@ -4,13 +4,13 @@ resource "github_actions_secret" "supabase_access_token" {
   value       = var.supabase_access_token
 }
 
-resource "github_actions_environment_secret" "project_id" {
+resource "github_actions_environment_variable" "project_id" {
   for_each = local.deployment_environments
 
-  repository  = github_repository.sabr.name
-  environment = github_repository_environment.environment[each.key].environment
-  secret_name = "SUPABASE_PROJECT_ID"
-  value       = data.tfe_outputs.supabase[each.key].nonsensitive_values["project_id"]
+  repository    = github_repository.sabr.name
+  environment   = github_repository_environment.environment[each.key].environment
+  variable_name = "SUPABASE_PROJECT_ID"
+  value         = data.tfe_outputs.supabase[each.key].nonsensitive_values["project_id"]
 }
 
 resource "github_actions_environment_secret" "db_password" {
