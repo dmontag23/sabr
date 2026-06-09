0d84a2f548d9af254e45ffdde25fe3a466cf2a6c
Dillon Montag <dmontag23@gmail.com>
2026-06-09 21:41:00 +0100

    Add expo public env vars to GitHub envs

--- infra/github/secrets.tf
+++ infra/github/secrets.tf
@@ -4,7 +4,16 @@ resource "github_actions_secret" "supabase_access_token" {
   value       = var.supabase_access_token
 }
 
-resource "github_actions_environment_variable" "project_id" {
+resource "github_actions_environment_secret" "supabase_db_password" {
+  for_each = local.deployment_environments
+
+  repository  = github_repository.sabr.name
+  environment = github_repository_environment.environment[each.key].environment
+  secret_name = "SUPABASE_DB_PASSWORD"
+  value       = data.tfe_outputs.supabase[each.key].values["database_password"]
+}
+
+resource "github_actions_environment_variable" "supabase_project_id" {
   for_each = local.deployment_environments
 
   repository    = github_repository.sabr.name
@@ -13,11 +22,20 @@ resource "github_actions_environment_variable" "project_id" {
   value         = data.tfe_outputs.supabase[each.key].nonsensitive_values["project_id"]
 }
 
-resource "github_actions_environment_secret" "db_password" {
+resource "github_actions_environment_variable" "supabase_publishable_key" {
   for_each = local.deployment_environments
 
-  repository  = github_repository.sabr.name
-  environment = github_repository_environment.environment[each.key].environment
-  secret_name = "SUPABASE_DB_PASSWORD"
-  value       = data.tfe_outputs.supabase[each.key].values["database_password"]
+  repository    = github_repository.sabr.name
+  environment   = github_repository_environment.environment[each.key].environment
+  variable_name = "EXPO_PUBLIC_SUPABASE_PUBLISHABLE_KEY"
+  value         = data.tfe_outputs.supabase[each.key].values["publishable_key"]
+}
+
+resource "github_actions_environment_variable" "supabase_url" {
+  for_each = local.deployment_environments
+
+  repository    = github_repository.sabr.name
+  environment   = github_repository_environment.environment[each.key].environment
+  variable_name = "EXPO_PUBLIC_SUPABASE_URL"
+  value         = data.tfe_outputs.supabase[each.key].nonsensitive_values["url"]
 }
--- infra/supabase/data.tf
+++ infra/supabase/data.tf
@@ -1,3 +1,7 @@
+data "supabase_apikeys" "sabr" {
+  project_ref = supabase_project.sabr.id
+}
+
 data "tfe_outputs" "resend" {
   organization = "sabs-apps"
   workspace    = "sabr-resend"
