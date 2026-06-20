514b6cd50f71b11a449e46ed3de70d796b811f38
Dillon Montag <dmontag23@gmail.com>
2026-06-20 02:30:05 +0100

    Increase max polling attempts

--- app/.maestro/scripts/extractEmailContent.js
+++ app/.maestro/scripts/extractEmailContent.js
@@ -52,7 +52,7 @@ const extractMailpitEmailContent = () => {
 const poll = (
   requestFn,
   shouldStopPolling,
-  { attempt = 1, maxAttempts = 25 } = {},
+  { attempt = 1, maxAttempts = 5000 } = {},
 ) => {
   const value = requestFn();
   if (shouldStopPolling(value)) return value;
